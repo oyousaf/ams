eb8abffc0b5c8d6f6ec4f4feaed05c33b530283c
Omar ♔ <o_yousaf@live.co.uk>
2024-11-09 12:44:07 +0000

    squashing bugs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'ev-database.org',
+        hostname: 'cdn.elferspot.com',
         pathname: '/**',
       },
       {

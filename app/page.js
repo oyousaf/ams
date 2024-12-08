49f2f547c8c0c8b00eec51cd4d02c3297a9d9e3a
Omar ♔ <o_yousaf@live.co.uk>
2024-12-08 11:16:24 +0000

    squashing bugs

--- app/page.js
+++ app/page.js
@@ -5,20 +5,17 @@ import About from "./components/About";
 import LatestCars from "./components/LatestCars";
 import Reviews from "./components/Reviews";
 import ScrollToTop from "./components/ScrollToTop";
-
-import Snowfall from "react-snowfall";
+import Snow from "./components/Snow";
 
 const Home = () => {
-  const isDecember = new Date().getMonth() === 11;
-
   return (
     <div>
-      {isDecember && <Snowfall style={{ position: "fixed", zIndex: 40 }} />}
       <HeroSection />
       <About />
       <LatestCars />
       <Reviews />
       <ScrollToTop />
+      <Snow />
     </div>
   );
 };

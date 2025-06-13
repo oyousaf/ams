50588b016223ef86bb60fdfc4ee509a38f624749
Omar 🍉 <o_yousaf@live.co.uk>
2025-06-13 19:18:04 +0100

    optimising dashboard

--- app/dashboard/CarList.jsx
+++ app/dashboard/CarList.jsx
@@ -1,8 +1,12 @@
+// app/dashboard/CarList.jsx
+
+"use client";
+
 import React, { memo } from "react";
 import CarListItem from "./CarListItem";
 
 const CarList = ({ cars, setCars, fetchCars }) => (
-  <div className="max-w-7xl mx-auto space-y-4 p-4">
+  <div className="max-w-7xl mx-auto space-y-4 p-4 overflow-y-auto max-h-[70vh] pr-2 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
     <ul>
       {cars.map((car) => (
         <CarListItem

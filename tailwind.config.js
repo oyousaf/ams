a24b755646023311f89c4271ccbfedb4177161b8
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-28 17:11:57 +0000

    migrated to tailwind v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      boxShadow: {
-        rosePulse: "0 0 10px #f43f5e, 0 0 30px #f43f5e66",
-      },
-    },
-  },
-  plugins: [require("tailwind-scrollbar")],
 };

bcb2ecc53670ad83a08ea5a0a30c006bb1b8e031
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-29 17:06:37 +0000

    a11y fixed

--- app/components/Divider.jsx
+++ app/components/Divider.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Divider = ({ className = "" }) => {
   return (
     <div
-      role="seperator"
       className={`w-full h-0.5 my-6 bg-linear-to-r from-transparent via-rose-400 to-transparent shadow-[0_0_6px_1px_rgba(244,63,94,0.4)] ${className}`}
     />
   );

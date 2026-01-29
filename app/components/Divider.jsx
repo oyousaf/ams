aedf686153669c3819030e174c55d1c69af769a1
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-29 17:08:21 +0000

    optimising a11y

--- app/components/Divider.jsx
+++ app/components/Divider.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Divider = ({ className = "" }) => {
   return (
     <div
+      aria-hidden="true"
       className={`w-full h-0.5 my-6 bg-linear-to-r from-transparent via-rose-400 to-transparent shadow-[0_0_6px_1px_rgba(244,63,94,0.4)] ${className}`}
     />
   );

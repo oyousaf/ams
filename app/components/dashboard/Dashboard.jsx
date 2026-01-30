d1d6c74d0dd66da885ea3ec82f93dbd85db2d67f
Omar 🍉 <o_yousaf@live.co.uk>
2026-01-30 16:44:58 +0000

    removed layout shift

--- app/components/dashboard/Dashboard.jsx
+++ app/components/dashboard/Dashboard.jsx
@@ -303,6 +303,7 @@ export default function Dashboard() {
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: -40 }}
               transition={{ duration: 0.25 }}
+              className="flex-1"
             >
               <AddCarForm
                 setCars={setCars}
